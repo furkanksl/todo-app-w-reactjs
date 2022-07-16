8ae1cf3f342cb34ef297d6607a1fc9c76d32a304
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-07-16 17:12:08 +0300

    Update todos with static data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import Todo from "./components/Todo";
+
+var todos = [
+  {
+    id: 1,
+    text: "Clean room",
+  },
+  {
+    id: 2,
+    text: "Make your bed",
+  },
+  {
+    id: 3,
+    text: "Go to gym",
+  },
+  {
+    id: 4,
+    text: "Wash the dishes",
+  },
+];
+
+const todoItems = todos.map((todo) => (
+  <li key={todo.id}>
+    <Todo text={todo.text} id={todo.id} />
+  </li>
+));
+
 function App() {
   return (
-    <div >
-      HELLO WORLD!
+    <div className="container">
+      <h1> My Todos </h1>
+      <ul>{todoItems}</ul>
     </div>
   );
 }
